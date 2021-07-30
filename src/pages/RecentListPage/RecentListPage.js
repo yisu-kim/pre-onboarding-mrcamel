fcdf84fdcb557f2820020cae46dafe485fdc9546
JuYoung <ken44929@gmail.com>
2021-07-30 19:42:14 +0900

    Feat: #23 Add RecentListPage UI

---
+++ src/pages/RecentListPage/RecentListPage.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import BrandFilterMenu from "../../components/BrandFilterMenu/BrandFilterMenu";
+import DislikeFilter from "../../components/DislikeFilter/DisLikeFilter";
+import { style } from "./RecentListPageStyle";
+import { ORIGINAL_DATA } from "../../utils/constants";
+import { Row, Col, Card } from "antd";
+const { Meta } = Card;
+const { RecentListContainer, ListTitle } = style;
+
+export default class RecentListPage extends Component {
+  state = {
+    datas: [],
+    checked: [],
+  };
+
+  loadData = async () => {
+    const data = await ORIGINAL_DATA;
+    this.setState({
+      datas: data,
+    });
+  };
+
+  handleFilters = (checked) => {
+    this.setState({
+      checked: checked,
+    });
+  };
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  render() {
+    const { datas, checked } = this.state;
+    let filteredList;
+
+    if (checked.length === 0) filteredList = datas;
+    else filteredList = datas.filter((data) => checked.includes(data.brand));
+    return (
+      <div>
+        <RecentListContainer>
+          <ListTitle>상품 조회 목록 페이지</ListTitle>
+
+          <Row gutter={[16, 16]}>
+            <Col lg={12} xs={24}>
+              <BrandFilterMenu handleFilters={this.handleFilters} />
+            </Col>
+            <Col lg={12} xs={24}>
+              <DislikeFilter />
+            </Col>
+          </Row>
+
+          <Row gutter={[16, 16]}>
+            {filteredList.map((data) => (
+              <Col lg={6} md={8} xs={24}>
+                <Card
+                  hoverable={true}
+                  cover={
+                    <img
+                      alt="example"
+                      style={cardImageStyle}
+                      src={`${data.imgUrl}`}
+                    />
+                  }
+                >
+                  <Meta title={data.title} description={`${data.brand}`} />
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </RecentListContainer>
+      </div>
+    );
+  }
+}
+
+const cardImageStyle = {
+  height: "150px",
+};
---
+++ src/pages/RecentListPage/RecentListPageStyle.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const RecentListContainer = styled.div`
+  max-width: 1100px;
+  margin: 2rem auto;
+  padding: 2rem;
+  text-align
+`;
+
+export const ListTitle = styled.h3`
+  font-weight: 500;
+  font-size: 30px;
+  padding: 20px 0;
+`;
+
+export const style = {
+  RecentListContainer,
+  ListTitle,
+};
