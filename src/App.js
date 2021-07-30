3822b1151ccda1f3b898486ba7029b25341789d1
yisu-kim <yisu.kim.dev@outlook.com>
2021-07-30 13:21:57 +0900

    Chore: Test relative path

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-function App() {
-  return <div className="App"></div>;
+import React, { Component } from "react";
+import ProductList from "./pages/ProductListPage";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ProductList />
+      </div>
+    );
+  }
 }
 
 export default App;
