c370851fcb10bd0306190ddba0b168dbf4af5648
JuYoung <ken44929@gmail.com>
2021-07-30 13:52:38 +0900

    Feat: #24 Add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
 import ProductList from "./pages/ProductListPage";
+import ProductDetailPage from "./pages/ProductDetailPage";
+import RecentListPage from "./pages/RecentListPage";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <ProductList />
+        <Route exact path='/' component={ProductList} />
+        <Route path='/:productId' component={ProductDetailPage} />
+        <Route path='/recent-list' component={RecentListPage} />
       </div>
     );
   }
