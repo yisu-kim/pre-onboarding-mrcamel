79a4236a1fd3b59200d29cf9355ae0521252459e
JuYoung <ken44929@gmail.com>
2021-07-31 01:21:01 +0900

    Fix: #43 Change primary color, fix absolute-paths

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,25 @@
 const { override, addWebpackAlias } = require("customize-cra");
+const addLessLoader = require("customize-cra-less-loader");
 const path = require("path");
 
 module.exports = override(
+  addLessLoader({
+    lessLoaderOptions: {
+      lessOptions: {
+        modifyVars: {
+          "primary-color": "#1f29f0",
+          "link-color": "#1f29f0",
+          "border-radius-base": "2px",
+        },
+        javascriptEnabled: true,
+      },
+    },
+  }),
   addWebpackAlias({
     "@": path.resolve(__dirname, "src"),
-    "@pages": path.resolve(__dirname, "src/pages"),
     "@components": path.resolve(__dirname, "src/components"),
     "@styles": path.resolve(__dirname, "src/styles"),
+    "@pages": path.resolve(__dirname, "src/pages"),
     "@utils": path.resolve(__dirname, "src/utils"),
   }),
 );
