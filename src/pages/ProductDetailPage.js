ec46caa5c52382108ab7428bcf533253b1cf292f
JuYoung <ken44929@gmail.com>
2021-07-30 11:19:39 +0900

    Feat: #20 directory-structure-setup

---
+++ src/pages/ProductDetailPage.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class ProductDetailPage extends Component {
+  render() {
+    return <div>ProductDetailPage</div>;
+  }
+}
---
+++ src/pages/ProductListPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Product } from "@/components/Product";
+
+export default class ProductList extends Component {
+  render() {
+    return (
+      <div>
+        <Link to='/'>datas</Link>
+        <Product>datas</Product>
+        <Product>datas</Product>
+        <Product>datas</Product>
+      </div>
+    );
+  }
+}
