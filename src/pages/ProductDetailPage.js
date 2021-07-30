6e5b02f380d7c4e78767dc829d0b65ac3104b623
yh1120 <yonghun1120@gmail.com>
2021-07-30 14:24:30 +0900

    FEAT: #26 load original data with id and imgUrl
    constants의 originalData 상수에 원데이터를 가공한 배열을 불러옴.
    순서대로 id 부여, id에 따른 다른 imgUrl 생성.

--- src/pages/ProductDetailPage.js
+++ src/pages/ProductDetailPage.js
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
+import { originalData } from "../utils/constants";
 
 export default class ProductDetailPage extends Component {
   render() {
-    return <div>ProductDetailPage</div>;
+    return (
+      <div>
+        <div>{originalData[1].title}</div>
+        <div>{originalData[1].brand}</div>
+        <div>{originalData[1].price}</div>
+        <div>{originalData[1].id}</div>
+        <img src={originalData[1].imgUrl} alt="productImage" />
+      </div>
+    );
   }
 }
