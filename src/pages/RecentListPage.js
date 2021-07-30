28ba86534a6d20c8c7fd668eefddda0f986c307a
yisu-kim <yisu.kim.dev@outlook.com>
2021-07-30 15:48:49 +0900

    Feat: #30 Add DislikeFilter component
    Handling checkbox checked state

--- src/pages/RecentListPage.js
+++ src/pages/RecentListPage.js
@@ -3,11 +3,25 @@ import BrandFilterMenu from "../components/BrandFilterMenu";
 import DislikeFilter from "../components/DislikeFilter";
 
 export default class RecentListPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      onlyInterestingProduct: false,
+    };
+    this.handleDislikeFilter = this.handleDislikeFilter.bind(this);
+  }
+
+  handleDislikeFilter(checked) {
+    this.setState({
+      onlyInterestingProduct: checked,
+    });
+  }
+
   render() {
     return (
       <div>
         <div>
-          <DislikeFilter />
+          <DislikeFilter handleDislikeFilter={this.handleDislikeFilter} />
           <BrandFilterMenu />
         </div>
       </div>
