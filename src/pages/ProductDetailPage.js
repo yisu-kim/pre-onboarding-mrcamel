9dddbe55de435a0edd12e2581de184149d62cb59
yh1120 <yonghun1120@gmail.com>
2021-07-31 11:38:46 +0900

    FIX : #33 random function and history push

--- src/pages/ProductDetailPage.js
+++ src/pages/ProductDetailPage.js
@@ -7,60 +7,87 @@ import {
   INTEREST_LIST,
 } from "../utils/constants";
 import propTypes from "prop-types";
-import { Link } from "react-router-dom";
 
 class ProductDetailPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      productId: -1,
+      productId: "-1",
+      original_data: ORIGINAL_DATA,
     };
   }
 
-  static getDerivedStateFromProps(nextProps) {
-    if (
-      parseInt(nextProps.match.params.productId) >= MIN_PRODUCT_ID &&
-      parseInt(nextProps.match.params.productId) < MAX_PRODUCT_ID
-    )
-      return { productId: parseInt(nextProps.match.params.productId) };
+  static getDerivedStateFromProps(nextProps, prevState) {
+    const productId = nextProps.match.params.productId;
+    if (prevState.productId !== "-1") {
+      return { productId: parseInt(productId) };
+    } else if (
+      (parseInt(productId) >= MIN_PRODUCT_ID &&
+        parseInt(productId) < MAX_PRODUCT_ID) ||
+      prevState.productId !== parseInt(productId)
+    ) {
+      return { productId: parseInt(productId) };
+    }
     return null;
   }
+
   render() {
-    const { productId } = this.state;
-    const handleRandom = () => {
-      if (!LOCAL_STORAGE.get("interestList")) {
-        LOCAL_STORAGE.set("interestList", INTEREST_LIST);
+    const { productId, original_data } = this.state;
+
+    const randomProduct = (interestList) => {
+      let temp = productId;
+      while (temp === productId) {
+        temp = Math.floor(Math.random() * interestList.length);
       }
-      const interestList = LOCAL_STORAGE.get("interestList");
+      return interestList[temp];
+    };
+
+    const handleRandom = async () => {
+      let interestList = await LOCAL_STORAGE.get("interestList");
+      if (interestList === null) {
+        await LOCAL_STORAGE.set("interestList", INTEREST_LIST);
+      }
+      interestList = await LOCAL_STORAGE.get("interestList");
       if (interestList.length <= 0) {
         return;
       }
-      const randomProduct = Math.floor(Math.random() * interestList.length);
-      return `/product/${interestList[randomProduct]}`;
+      const nextProductId = randomProduct(interestList);
+      this.props.history.push(`/product/${nextProductId}`);
     };
-    const handleDislike = () => {
-      if (!LOCAL_STORAGE.get("interestList")) {
-        LOCAL_STORAGE.set("interestList", INTEREST_LIST);
+
+    const handleDislike = async () => {
+      let interestList = await LOCAL_STORAGE.get("interestList");
+      if (interestList === null) {
+        await LOCAL_STORAGE.set("interestList", INTEREST_LIST);
       }
-      const interestList = LOCAL_STORAGE.get("interestList");
-      const tempArray = [];
-      Array.from(interestList).map((element) => {
-        if (element !== productId) {
-          tempArray.push(element);
+      interestList = await LOCAL_STORAGE.get("interestList");
+      if (interestList.length <= 0) {
+        return;
+      }
+      let tempArray = [];
+      for (let i = 0; i < interestList.length; i++) {
+        if (interestList[i] === productId) {
+          tempArray = interestList
+            .slice(0, i)
+            .concat(interestList.slice(i + 1));
+          break;
         }
-      });
-      console.log(tempArray);
-      LOCAL_STORAGE.set("interestList", tempArray);
+      }
+      await LOCAL_STORAGE.set("interestList", tempArray);
+      const nextProductId = randomProduct(tempArray);
+      this.setState({ productId: `${nextProductId}` });
+      this.props.history.push(`/product/${nextProductId}`);
     };
+
     return (
       <div>
         {productId !== -1 ? (
           <div>
-            <div>{ORIGINAL_DATA[productId].title}</div>
-            <div>{ORIGINAL_DATA[productId].brand}</div>
-            <div>{ORIGINAL_DATA[productId].price}</div>
-            <img src={ORIGINAL_DATA[productId].imgUrl} alt="productImage" />
-            <Link to={handleRandom}>Random</Link>
+            <img src={original_data[productId].imgUrl} alt="productImage" />
+            <div>{original_data[productId].title}</div>
+            <div>{original_data[productId].brand}</div>
+            <div>{original_data[productId].price}</div>
+            <button onClick={handleRandom}>Random</button>
             <button onClick={handleDislike}>Dislike</button>
           </div>
         ) : (
@@ -74,8 +101,11 @@ class ProductDetailPage extends Component {
 ProductDetailPage.propTypes = {
   match: propTypes.shape({
     params: propTypes.shape({
-      productId: propTypes.number,
+      productId: propTypes.string,
     }),
+  }),
+  history: propTypes.shape({
+    push: propTypes.func,
   }),
 };
 
