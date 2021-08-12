991657e4e5f3414474c0e89f510626055b5d1575
UlongChaS2 <jyujung2@gmail.com>
2021-08-13 00:01:16 +0900

    Feat: add reset RecentList

--- src/pages/RecentListPage/RecentListPage.js
+++ src/pages/RecentListPage/RecentListPage.js
@@ -21,11 +21,24 @@ export default class RecentListPage extends Component {
     onlyInterestingProduct: false,
     filteredDatas: [],
     priceChecked: false,
+    date: new Date(),
+  };
+
+  update = () => {
+    this.setState({
+      date: new Date(),
+    });
   };
 
   getRecentList = () => {
     this.setState({
       datas: LOCAL_STORAGE.get("recentList"),
+    });
+  };
+
+  clearRecentList = () => {
+    this.setState({
+      datas: LOCAL_STORAGE.set("recentList", []),
     });
   };
 
@@ -53,22 +66,40 @@ export default class RecentListPage extends Component {
     });
   };
 
-  goProductListPage = () => {
-    this.props.history.push("/product");
-  };
+  // goProductListPage = () => {
+  //   this.props.history.push("/product");
+  // };
 
   componentDidMount() {
     this.getRecentList();
+
+    setInterval(this.update, 1000);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const hour = this.state.date.getHours();
+    const minute = this.state.date.getMinutes();
+    const second = this.state.date.getSeconds();
+
+    if (hour + minute + second === 0) {
+      if (prevState.date !== this.state.date) {
+        LOCAL_STORAGE.set("recentList", []),
+          this.setState({
+            datas: LOCAL_STORAGE.get("recentList"),
+          });
+      }
+    }
   }
 
   render() {
     const { datas, checked, onlyInterestingProduct, priceChecked } = this.state;
+
     let products = datas;
     products = products.map((data) => {
       const originalData = getOriginalInfo(data.id);
       return { ...data, price: originalData.price };
     });
-    console.log(products);
+
     let filteredList;
 
     const compareFunction = (a, b) => {
@@ -140,36 +171,37 @@ export default class RecentListPage extends Component {
           </Row>
 
           <Row gutter={[16, 16]}>
-            {filteredList.map((data) => {
-              const originalData = getOriginalInfo(data.id);
+            {filteredList &&
+              filteredList.map((data) => {
+                const originalData = getOriginalInfo(data.id);
 
-              return (
-                <Col lg={6} md={8} xs={24} key={data.id}>
-                  <Link
-                    to={(location) => {
-                      if (data.dislike) {
-                        return { ...location };
-                      }
-                      return {
-                        ...location,
-                        pathname: `/product/${data.id}`,
-                      };
-                    }}
-                    onClick={() => this.handleAccessPopup(data.dislike)}
-                  >
-                    <Card
-                      hoverable={true}
-                      cover={<img alt="example" src={originalData.imgUrl} />}
+                return (
+                  <Col lg={6} md={8} xs={24} key={data.id}>
+                    <Link
+                      to={(location) => {
+                        if (data.dislike) {
+                          return { ...location };
+                        }
+                        return {
+                          ...location,
+                          pathname: `/product/${data.id}`,
+                        };
+                      }}
+                      onClick={() => this.handleAccessPopup(data.dislike)}
                     >
-                      <Meta
-                        title={originalData.title}
-                        description={originalData.price}
-                      />
-                    </Card>
-                  </Link>
-                </Col>
-              );
-            })}
+                      <Card
+                        hoverable={true}
+                        cover={<img alt="example" src={originalData.imgUrl} />}
+                      >
+                        <Meta
+                          title={originalData.title}
+                          description={originalData.brand}
+                        />
+                      </Card>
+                    </Link>
+                  </Col>
+                );
+              })}
           </Row>
         </RecentListContainer>
       </div>
