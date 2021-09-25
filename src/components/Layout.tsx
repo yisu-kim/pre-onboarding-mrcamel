1726af185f507d80bea53d8596001a2548eb5066
yisu-kim <yisu.kim.dev@outlook.com>
2021-09-25 20:59:45 +0900

    Refactor: Apply ts to common components

---
+++ src/components/Layout.tsx
@@ -0,0 +1,26 @@
+import { Component } from 'react';
+import { Col, Row } from 'antd';
+import Header from 'components/Header';
+
+type LayoutProps = {
+  menu: JSX.Element;
+  children: JSX.Element;
+};
+
+class Layout extends Component<LayoutProps> {
+  render(): JSX.Element {
+    return (
+      <Row gutter={[0, 16]} justify='center'>
+        <Col xs={22} sm={20} md={18} xl={16}>
+          <Header menu={this.props.menu}></Header>
+        </Col>
+
+        <Col xs={22} sm={20} md={18} xl={16}>
+          <div>{this.props.children}</div>
+        </Col>
+      </Row>
+    );
+  }
+}
+
+export default Layout;
